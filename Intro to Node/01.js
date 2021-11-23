a2e8625207388cf0c313433a2996882991261e87
Obaid Ur Rahman <obaidkhan972@gmail.com>
2021-11-23 23:54:25 +0500

    Running file with Node

---
+++ Intro to Node/01.js
@@ -0,0 +1,18 @@
+//  GLOBALS -NO WINDOWS (Browswer) NEEDED
+
+//  __dirname   -   Path to current directory. 
+//  __filename  -   filename. 
+//  require     -   function to use modules. 
+//  module      -   info abot current module (file). 
+//  process     -   info about env where the progam is being executed. 
+
+//  To run JS file without window, make sure you have installed NodeJS
+//  go to the file directory, open cmd there, run Node <fileName> 01.js (with / without extension).
+//  OR open cmd, run Node <directory/filename>
+
+console.log(__dirname);
+console.log(__filename);
+// console.log(module);
+// console.log(process);
+console.log("Try runnung them by typing 'node <filename>' in the terminal.");
+//Remove the comment from other console statement and check the details printed.
